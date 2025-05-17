d5e7ec2421f7e2a7e120488a3e9ee28a139fca2f
LOUSHIABBAS <87332573+LOUSHIABBAS@users.noreply.github.com>
2025-05-16 21:08:57 -0400

    first mock up complete, ui complete, with firebase integration. Android functional, iOS need work/cant build.

--- healthai_app/android/app/build.gradle.kts
+++ healthai_app/android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
@@ -9,15 +9,16 @@ plugins {
 android {
     namespace = "com.example.healthai_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
+        freeCompilerArgs += listOf("-Xjvm-default=all")
     }
 
     defaultConfig {
--- healthai_app/android/build.gradle.kts
+++ healthai_app/android/build.gradle.kts
@@ -21,6 +21,6 @@ tasks.register<Delete>("clean") {
 }
 
 plugins {
-    // ... other plugins
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
+
